1dbac93ef7e11f5a97b6b01183a56b23868a4028
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2026-03-05 11:20:28 +0000

    create dto ProductRequest

--- produit_service/src/main/java/com/produit_service/dto/requests/ProductRequest.java
+++ produit_service/src/main/java/com/produit_service/dto/requests/ProductRequest.java
@@ -1,4 +1,23 @@
-package com.produit_service.dto.requests.responses;
+package com.produit_service.dto.requests;
 
-public class ProductDTO {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductRequest {
+    @NotBlank(message = "Le nom est obligatoire")
+    private String name;
+    
+    @Positive(message = "Le prix doit être supérieur à 0")
+    private double price;
+    
+    @Min(value = 0, message = "Le stock doit être supérieur ou égal à 0")
+    private int quantity;
+    
+    @NotBlank(message = "La catégorie est obligatoire")
+    private String category;
 }
