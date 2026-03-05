6a5494082361e108612c83b542f92591bafb1e62
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2026-03-05 11:26:47 +0000

    Créer gestion des exceptions

---
+++ produit_service/src/main/java/com/produit_service/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.produit_service.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+}
