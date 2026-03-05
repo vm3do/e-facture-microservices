ecb98494651f3380d8ffed01e63a199e8c412897
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2026-03-05 11:22:48 +0000

    add ProductRepository

--- produit_service/src/main/java/com/produit_service/repository/ProductRepository.java
+++ produit_service/src/main/java/com/produit_service/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.produit_service.repository;
 
-public class ProductRepository {
+import com.produit_service.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    
 }
