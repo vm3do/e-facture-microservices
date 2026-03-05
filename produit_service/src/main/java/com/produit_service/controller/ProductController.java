014fbf48c06bbeba1beb6dc46d7dd1041111bfed
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2026-03-05 11:27:53 +0000

    create controller package

---
+++ produit_service/src/main/java/com/produit_service/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.produit_service.controller;
+
+import com.produit_service.dto.requests.ProductRequest;
+import com.produit_service.dto.responses.ProductResponse;
+import com.produit_service.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/produits")
+@RequiredArgsConstructor
+public class ProductController {
+    
+    private final ProductService productService;
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@Valid @RequestBody ProductRequest request) {
+        return productService.createProduct(request);
+    }
+}
