86e62ca17447565536df30b81f61221a89145ef2
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2026-03-05 11:21:55 +0000

    create productMapper

--- produit_service/src/main/java/com/produit_service/mapper/ProductMapper.java
+++ produit_service/src/main/java/com/produit_service/mapper/ProductMapper.java
@@ -1,4 +1,12 @@
 package com.produit_service.mapper;
 
+import com.produit_service.dto.requests.ProductRequest;
+import com.produit_service.dto.responses.ProductResponse;
+import com.produit_service.entity.Product;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ProductMapper {
+    ProductResponse toResponse(Product product);
+    Product toEntity(ProductRequest request);
 }
